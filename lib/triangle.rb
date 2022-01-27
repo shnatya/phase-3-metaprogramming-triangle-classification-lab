71d483dd89b5a55a5c6e062157ac14e29ff9d9f0
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-27 10:03:23 -0800

    commit tree 6add16a87f189f5aacc98f54d206c612e628c583

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require "pry"
 class Triangle
-  # write code here
+  # triangle code
+  attr_reader :s1, :s2, :s3
+  #@@triangle_types = [:equilateral, :isosceles, :scalene]
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+   #binding.pry
+    if ![@s1, @s2, @s3].include?(0) && (@s1+@s2) > @s3 && (@s2+@s3) > @s1 && (@s1+@s3) > @s2
+      #binding.pry
+      if @s1 == @s2 && @s2 == @s3 && @s1 == @s3
+        #binding.pry
+        :equilateral
+      elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
+        :isosceles
+      else [@s1, @s2, @s3].uniq == nil
+        :scalene
+      end
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
